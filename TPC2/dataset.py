0eaed07103b8733a5f8d007fcd0c129c00eae374
Duarte Faria <a95609@alunos.uminho.pt>
2025-02-20 21:03:29 +0000

    TPC2 update

--- TPC2/dataset.py
+++ TPC2/dataset.py
@@ -1,6 +1,6 @@
 import re
 
-file = open("./obras.csv", "r", encoding="utf-8")
+file = open("TPC2\obras.csv", "r", encoding="utf-8")
 
 ignoreHeader = file.readline()
 
@@ -14,6 +14,23 @@ while line:
         newLine = file.readLine().strip()
         if not newLine:
             break
-
+        line += " " + newLine
+        reg = re.split(r';(?=(?:[^"]*"[^"]*")*[^"]*$)', line)
+        
+    compositor = f"{reg[4]}"
+    compositores.append(compositor)        
+    periodo = f"{reg[3]}"
+    numObrasPeriodo[periodo] = numObrasPeriodo.get(periodo, 0) + 1
+    titulo = f"{reg[0]}"
+    obrasPeriodo[periodo] = obrasPeriodo.get(periodo, []) + [titulo]
+    
+    
+compositores.sort()
+for periodo in obrasPeriodo:
+    obrasPeriodo[periodo].sort()
+    
+print(compositores)
+print(numObrasPorPeriodo)
+print(obrasPeriodo)
 
 
