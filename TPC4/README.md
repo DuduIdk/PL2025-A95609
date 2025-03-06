fc81a03d8074f714f8bb486ebd730729a54fd9c3
Duarte Faria <79994735+DuduIdk@users.noreply.github.com>
2025-03-06 21:37:10 +0000

    TPC4

---
+++ TPC4/README.md
@@ -0,0 +1,62 @@
+## TPC4 - Analisador léxico
+
+**Data:** 2025-02-28
+
+## Resumo: 
+Este TPC consiste em criar um analisador léxico para uma linguagem de query. Por exemplo:
+        #DBPedia: obras de Chuck Berry
+
+        SELECT ?nome ?desc WHERE {
+            ?s a dbo:MusicalArtist.
+            ?s foaf:name "Chuck Berry"@en.
+            ?w dbo:artist ?s.
+            ?w foaf:name ?nome.
+            ?w dbo:abstract ?desc.
+        } LIMIT 1000
+
+O resultado esperado seria:
+        COMMENT #DBPedia: obras de Chuck Berry
+        SELECT SELECT
+        VAR nome
+        VAR desc
+        WHERE WHERE
+        LBRAC {
+        VAR s
+        A a
+        PREF dbo:MusicalArtist
+        DOT .
+        VAR s
+        PREF foaf:name
+        STRING Chuck Berry
+        LANG @en
+        DOT .
+        VAR w
+        PREF dbo:artist
+        VAR s
+        DOT .
+        VAR w
+        PREF foaf:name
+        VAR nome
+        DOT .
+        VAR w
+        PREF dbo:abstract
+        VAR desc
+        DOT .
+        RBRAC }
+        LIMIT LIMIT
+        INT 1000
+
+## Resultados
+- [Código fonte ()]()
+- [Ficheiro Markdown]()
+- [Ficheiro HTML]()
+
+## Autor
+
+**Nome:** Duarte Alexandre Oliveira Faria
+
+**Número:** a95609
+
+**Fotografia:**
+
+![Fotografia do Autor](TPC1\20200928.jpg) 
--- TPC4/analex.py
+++ TPC4/analex.py
@@ -1,7 +1,19 @@
 import re
 import ply.lex as lex # type: ignore
 
-tokens = {
+string = """
+#DBPedia: obras de Chuck Berry
+
+SELECT ?nome ?desc WHERE {
+    ?s a dbo:MusicalArtist.
+    ?s foaf:name "Chuck Berry"@en.
+    ?w dbo:artist ?s.
+    ?w foaf:name ?nome.
+    ?w dbo:abstract ?desc.
+} LIMIT 1000
+"""
+
+tokens = [
     'SELECT',
     'WHERE',
     'LIMIT',
@@ -15,7 +27,7 @@ tokens = {
     'INT',
     'STRING',
     'PREF'
-}
+]
 
 t_SELECT = r'(?i:SELECT)'
 t_WHERE = r'(?i:WHERE)'
@@ -25,12 +37,42 @@ t_LBRAC = r'\{'
 t_RBRAC = r'\}'
 
 def t_VAR(t):
-    r'\?w+'
-    t.value = t.value.strip('?')
+    r'\?[a-zA-Z_][a-zA-Z0-9_]*'
+    t.value = t.value[1:]
     return t
 
-t_A = r'a'
-t_LANG= r'@[a-z]{2,3}([a-z]{2,3})'
-t_PREF = r'[a-zA-Z][a-zA-Z]'
+def t_A(t):
+    r'\ba\b'
+    return t
 
+t_LANG = r'@[a-z]{2,3}(-[a-z]{2,3})?'
+t_PREF = r'[a-zA-Z_][a-zA-Z0-9_-]*:[a-zA-Z_][a-zA-Z0-9_-]*'
 
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_STRING(t):
+    r'\"([^"\\]*(\\.[^"\\]*)*)\"'
+    t.value = t.value[1:-1]
+    return t
+
+t_COMMENT = r'\#.*'
+
+t_ignore = ' \t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+lexer.input(string)
+
+for tok in lexer:
+    print(tok.type, tok.value)
