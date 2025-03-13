44072f5dad8b44f2211ca91520feeb858d60cacb
Duarte Faria <79994735+DuduIdk@users.noreply.github.com>
2025-03-13 20:22:59 +0000

    TPC5(unfinished)

--- TPC5/vending.py
+++ TPC5/vending.py
@@ -1,73 +1,133 @@
+import ply.lex as lex
+from tabulate import tabulate
 import json
 import re
-import tabulate 
 
-STOCK_FILE = "stock.json"
 
-tokens = [
-    'LISTAR',
-    'MOEDA',
-    'SELECIONAR',
-    'SAIR'
+class MaquinaVending:
+
+    def __init__ (self, stock: dict):
+        self.lexer = lex.lex(module=self)
+        self.saldo = 0
+        self.stock = stock
+        self.on = True
+
+    
+
+    tokens = [
+        'LISTAR',
+        'MOEDA',
+        'SELECIONAR',
+        'SAIR'
     ]
 
-def t_LISTAR(t):
-    r'LISTAR'
-    
-    table = []
-    for cod, values in stock.items():
-        table.append([cod, values[0], values[1], values[2]])
+    def t_LISTAR(self, t):
+        r'LISTAR'
         
-    print(tabulate(table, headers = ["cod", "nome", "quantidade", "preço"]))
-    return t
+        table_data = []
+        for cod, values in self.stock.items():
+            table_data.append([cod, values[0], values[1], values[2]])
 
-def t_MOEDA(t):
-    r'MOEDA\s+((1|2|5|10|20|50)c|(1|2)e)(?:\s*,\s*)'
+        print(tabulate(table_data, headers=["Código", "Nome", "Quantidade", "Preço"]))
+
+        return t
     
-    for moeda in re.finditer(r'(?:(?P<cent>1|2|5|10|20|50)c|(?P<euro>1|2)e)', t.value):
-        if moeda.lastgroup == "cent":
-            saldo += int(moeda.group("cent"))
+
+    def t_MOEDA (self, t):
+        r"MOEDA\s+((2|5|10|20|50)c|(1|2)e)(?:\s*,\s*((2|5|10|20|50)c|(1|2)e))*"
+
+        for moeda in re.finditer(r'(?:(?P<cent>2|5|10|20|50)c|(?P<euro>1|2)e)', t.value):
+            if moeda.lastgroup == "cent":
+                self.saldo += int(moeda.group("cent"))
+            else:
+                self.saldo += int(moeda.group("euro")) * 100
+        printable_euro, printable_cent = divmod(self.saldo, 100)
+        print(f"Saldo = {printable_euro}e{printable_cent}c")
+
+        return t
+
+
+    def t_SELECIONAR (self, t):
+        r"SELECIONAR\s+.+"
+
+        codigo = t.value.replace(" ", "")[10:]
+        item = self.stock.get(codigo)
+        if not item:
+            print(f"Não existe nenhum produto com código {codigo}")
         else:
-            saldo += int(moeda.group("euro")) * 100
-            
-    euro, cent = divmod(saldo, 100)
-    print(f"Saldo = {euro}e{cent}c")
-    
-    return t
+            if item[2] > self.saldo:
+                print("Saldo insuficiente para satisfazer o seu pedido")
+                printable_euro_saldo, printable_cent_saldo = divmod(self.saldo, 100)
+                printable_euro_item, printable_cent_item = divmod(item[2], 100)
+                print(f"Saldo = {printable_euro_saldo}e{printable_cent_saldo}c; Pedido = {printable_euro_item}e{printable_cent_item}c")
+            else:
+                print(f"Pode retirar o produto dispensado \"{item[0]}\"")
+                self.saldo -= item[2]
+                printable_euro, printable_cent = divmod(self.saldo, 100)
+                print(f"Saldo = {printable_euro}e{printable_cent}c")
+                if item[1] == 1:
+                    del self.stock[codigo]
+                else:
+                    self.stock[codigo] = (item[0], item[1] - 1, item[2])
 
-def t_SELECIONAR(t):
-    r'SELECIONAR\s+.+'
-    
-    codigo = t.value.replace(" ", "")[10:]
-    produto = stock.get(codigo)
-    if not produto:
-        print(f"Não existe nenhum produto com código {codigo}")
-    elif produto[2] > saldo:
-        print("Saldo insuficiente para efetuar o seu pedido")
-        saldoEuro, saldoCent = divmod(saldo, 100)
-        produtoEuro, produtoCent = divmod(produto[2], 100)
-        print(f"Saldo = {saldoEuro}e{saldoCent}c; Pedido = {produtoEuro}e{produtoCent}c")
-        
-    else:
-        print(f"Pode retirar o seu produto: *{item[0]}*")
-        saldo -= produto[2]
-        euro, cent = divmod(saldo, 100)
-        
-        if produto[1] == 1:
-            del stock[codigo]
+        return t
+
+
+    def t_SAIR (self, t):
+        r'SAIR'
+
+        if self.saldo == 0:
+            print("Sem troco para dar")
         else:
-            stock[codigo] = (produto[0], produto[1] - 1, produto[2])
-            
-    return t
+            output = "Pode retirar o troco: "
+            saldo = self.saldo
+            moedas = [200, 100, 50, 20, 10, 5, 2]
+            i = 0
+            while (i < 2 and saldo != 0):
+                quoc, resto = divmod(saldo, moedas[i])
+                if quoc != 0:
+                    output += f"{quoc}x {moedas[i] / 100}e, "
+                saldo = resto
+                i += 1
 
-def load_stock():
-    try:
-        with open(STOCK_FILE, "r", encoding = "utf-8") as f:
-            json.load(f)
-    except  FileNotFoundError:
-        return []
+            while (saldo != 0):
+                quoc, resto = divmod(saldo, moedas[i])
+                if quoc != 0:
+                    output += f"{quoc}x {moedas[i]}c, "
+                saldo = resto
+                i += 1
+            print(output[:-2])
+        print("Até à próxima")
+        self.on = False
+        return t
 
-def save_stock():
-    with open(STOCK_FILE, "w", encoding = "utf-8") as f:
-        json.dump(stock, f, indent = 4)
 
+    t_ignore = ' \t'
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    def t_error(self, t):
+        print(f"Unsupported character '{t.value[:-1]}'")
+        t.lexer.skip(len(t.value))
+
+def main():
+    with open('stock.json', encoding="utf-8") as json_file:
+        data = json.load(json_file)
+    stock = {}
+    for item in data["stock"]:
+        key = item['cod']
+        value = (item['nome'], int(item['quant']), int(float(item['preco']) * 100))
+        stock[key] = value
+
+    maquina = MaquinaVending(stock)
+
+    while(maquina.on):
+        line = input()
+        maquina.lexer.input(line)
+        maquina.lexer.token()
+
+
+if __name__ == "__main__":
+    main()
